be3edb3541f67b2cd766148a281fac45058b0972
yodhasu <yodhasu@gmail.com>
2024-12-28 00:54:59 +0700

    modify home view on css href

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
 @endsection
 
 @section('css')
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/home.css">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 @endsection
 
 @section('js')
