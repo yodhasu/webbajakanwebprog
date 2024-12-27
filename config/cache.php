742a31dcb9be68d09ca7417d6b73f4a1b8aa48c7
yodhasu <yodhasu@gmail.com>
2024-12-28 02:31:51 +0700

    change vercel a lot2

--- config/cache.php
+++ config/cache.php
@@ -51,7 +51,7 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path' => env('CACHE_PATH', '/tmp/laravel/cache'),
         ],
 
         'memcached' => [
