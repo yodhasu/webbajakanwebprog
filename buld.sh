c116f037cfc981f4a1a7e4eaea8904538ca25df0
yodhasu <yodhasu@gmail.com>
2024-12-28 02:46:36 +0700

    change vercel a lot8

---
+++ buld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Run Laravel and npm commands
+composer install --no-dev
+php artisan key:generate
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+npm install
+npm run build
