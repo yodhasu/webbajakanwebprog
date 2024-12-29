03db44de11aa8162491e8fd1c7c5c27b4ab9d7a9
yodhasu <yodhasu@gmail.com>
2024-12-29 21:32:42 +0700

    try changin vercel, change build to function

--- api/index.php
+++ api/index.php
@@ -1,12 +1,3 @@
 <?php
 
 require __DIR__ . '/../public/index.php';
-require __DIR__ . '/../public/css/cinema-background-csjrmkuhfvvumb2q.jpg';
-// require __DIR__ . '/../public/css/Dartsinadartboard.jpg';
-// require __DIR__ . '/../public/css/home.css';
-require __DIR__ . '/../public/css/movie.css';
-require __DIR__ . '/../public/css/search.css';
-require __DIR__ . '/../public/css/style.css';
-// require __DIR__ . '/../public/css/testbackground.webp';
-// require __DIR__ . '/../public/css/wp10615928.jpg';
-require __DIR__ . '/../public/js/scroll.js';
