252178ba09a1b5312204dd69de897135da8318b0
yodhasu <yodhasu@gmail.com>
2024-12-29 19:39:39 +0700

    update2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') !== 'local') {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
